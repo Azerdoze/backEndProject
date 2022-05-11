1e75ac30789e09f4cf7707ea936b74f810945cea
Eurico <euricoabreuteles@gmail.com>
2022-05-11 21:31:52 +0000

    removed variables from call to db

--- backend/models/base.php
+++ backend/models/base.php
@@ -8,18 +8,18 @@ class Base {
     
     public function __construct() {
         // $this -> db = new PDO("mysql:host=localhost;dbname=projetobackend;charset=utf8mb4", "root", "");
-        $host = 'arthan-uthyl-reborn.c69jaifibnpl.eu-west-3.rds.amazonaws.com';
-        $db = 'projetobackend';
-        $port = '3306';
-        $charset = 'utf8mb4';
-        $user = 'eurico';
-        $pass = '12341234';
+        // $host = 'arthan-uthyl-reborn.c69jaifibnpl.eu-west-3.rds.amazonaws.com';
+        // $db = 'projetobackend';
+        // $port = '3306';
+        // $charset = 'utf8mb4';
+        // $user = 'eurico';
+        // $pass = '12341234';
         
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port;";
+        // $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port;";
         
-        $this -> db = new PDO($dsn, $user, $pass);
+        // $this -> db = new PDO($dsn, $user, $pass);
         // $this -> db = new PDO("mysql:host=$host;dbname=$db;charset=$charset;port=$port", "$user", "$pass");
-        // $this -> db = new PDO("mysql:host=arthan-uthyl-reborn.c69jaifibnpl.eu-west-3.rds.amazonaws.com;dbname=projetobackend;charset=utf8mb4;port=3306", "euricor", "12341234");
+        $this -> db = new PDO("mysql:host=arthan-uthyl-reborn.c69jaifibnpl.eu-west-3.rds.amazonaws.com;dbname=projetobackend;charset=utf8mb4;port=3306", "euricor", "12341234");
     }
 
     public function routeRequiresValidation() {
